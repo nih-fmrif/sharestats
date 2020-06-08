373f2c25b79edb992134699f6cdb772a27bdaf9f
David Siebo <zentraal@gmail.com>
2020-06-08 11:23:58 -0700

    setting PUBLIC_URL via an env-var for yarn build

---
+++ precache-manifest.2368c87f7271adb4ff3a94532e8e0729.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b0f9358897757c1d9d86932f80bfc9",
+    "url": "https://nih-fmrif.github.io/sharestats/index.html"
+  },
+  {
+    "revision": "a063c1f9735f86a0c7d3",
+    "url": "https://nih-fmrif.github.io/sharestats/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5862a81594c6c15519c3",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/2.44a7e903.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/2.44a7e903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a063c1f9735f86a0c7d3",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/main.49753fd0.chunk.js"
+  },
+  {
+    "revision": "a888652928d39bd29a45",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/runtime-main.dcc49af7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "https://nih-fmrif.github.io/sharestats/static/media/logo.5d5d9eef.svg"
+  }
+]);
