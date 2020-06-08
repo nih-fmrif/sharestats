85efc78b8f5bb6bd7f928e757eb20117f9afef1f
David Siebo <zentraal@gmail.com>
2020-06-08 10:24:56 -0700

    first react build

---
+++ precache-manifest.d98f0726fefce54fcaddcd72708cdf5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a694a30208c778970e9c592150c3a1a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "142e24e3b17911854429",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5862a81594c6c15519c3",
+    "url": "/static/js/2.44a7e903.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.44a7e903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "142e24e3b17911854429",
+    "url": "/static/js/main.b67c1e38.chunk.js"
+  },
+  {
+    "revision": "f1b4e2a75afe394a41a0",
+    "url": "/static/js/runtime-main.c9364430.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
