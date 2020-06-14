0f65218ee94c310cb51c78a4127c887a9bd89eae
David Siebo <zentraal@gmail.com>
2020-06-14 05:18:08 -0700

    logging the response object so we can take a look

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sharestats/precache-manifest.138bb51244927f420c39c85525503329.js"
+  "/sharestats/precache-manifest.ec271f46c424f3423e05a7132b4b4d43.js"
 );
 
 self.addEventListener('message', (event) => {
