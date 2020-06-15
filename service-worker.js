18787175e631315dc7c983f213f36b38ca3ff5ed
David Siebo <zentraal@gmail.com>
2020-06-15 13:47:26 -0700

    clean up drawer nav and add handler methods for nav push

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://nih-fmrif.github.io/sharestats/precache-manifest.1c5f51f5f4a1a6334da504bc71e7985e.js"
+  "https://nih-fmrif.github.io/sharestats/precache-manifest.03e8619470d28aea1a9b198d3c7ae1d8.js"
 );
 
 self.addEventListener('message', (event) => {
