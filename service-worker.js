cd4a9b709c73081cc19f9c362045f8038ec9adb5
David Siebo <zentraal@gmail.com>
2020-06-14 20:56:34 -0700

    move GoogleBtn component call to App.js

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://nih-fmrif.github.io/sharestats/precache-manifest.727df25f5a0b562291d354fc5d9b6b92.js"
+  "https://nih-fmrif.github.io/sharestats/precache-manifest.27516915205494097c993fc8675e3915.js"
 );
 
 self.addEventListener('message', (event) => {
