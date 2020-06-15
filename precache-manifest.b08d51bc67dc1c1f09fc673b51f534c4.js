fa8dd8a1dae1d99687152d786211cd2cd1e7e2d8
David Siebo <zentraal@gmail.com>
2020-06-15 14:47:32 -0700

    moving NavItems to separate component so we can use history

---
+++ precache-manifest.b08d51bc67dc1c1f09fc673b51f534c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821999c8d86116099b20c266ba3bfd9b",
+    "url": "https://nih-fmrif.github.io/sharestats/index.html"
+  },
+  {
+    "revision": "1ba4e2a43c9028637f54",
+    "url": "https://nih-fmrif.github.io/sharestats/static/css/main.d68c5cd1.chunk.css"
+  },
+  {
+    "revision": "d4f38f79fada3b6cad31",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/2.f63f190c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/2.f63f190c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ba4e2a43c9028637f54",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/main.cc0e5aae.chunk.js"
+  },
+  {
+    "revision": "a888652928d39bd29a45",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/runtime-main.dcc49af7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://nih-fmrif.github.io/sharestats/precache-manifest.0435ddc56340ea34e91267d38ae966b2.js"
+  "https://nih-fmrif.github.io/sharestats/precache-manifest.b08d51bc67dc1c1f09fc673b51f534c4.js"
 );
 
 self.addEventListener('message', (event) => {
