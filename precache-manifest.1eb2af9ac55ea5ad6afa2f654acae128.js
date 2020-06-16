623a84b1584350abe14c881222e513c7db343fba
David Siebo <zentraal@gmail.com>
2020-06-16 03:43:22 -0700

    search UI and graphs placeholder

---
+++ precache-manifest.1eb2af9ac55ea5ad6afa2f654acae128.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e56960ea2275f2340dbfc487b8f8ccf",
+    "url": "https://nih-fmrif.github.io/sharestats/index.html"
+  },
+  {
+    "revision": "26d44532737b0203441d",
+    "url": "https://nih-fmrif.github.io/sharestats/static/css/main.d68c5cd1.chunk.css"
+  },
+  {
+    "revision": "e3266117ce5462282c79",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/2.ad46a1b8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/2.ad46a1b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26d44532737b0203441d",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/main.0a9851e7.chunk.js"
+  },
+  {
+    "revision": "a888652928d39bd29a45",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/runtime-main.dcc49af7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://nih-fmrif.github.io/sharestats/precache-manifest.bbc0fa7f9fac5227859f599edc02f670.js"
+  "https://nih-fmrif.github.io/sharestats/precache-manifest.1eb2af9ac55ea5ad6afa2f654acae128.js"
 );
 
 self.addEventListener('message', (event) => {
