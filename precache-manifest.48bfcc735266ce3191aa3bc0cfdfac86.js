7685cf5d7718b99007452b9e112cf19585025053
David Siebo <zentraal@gmail.com>
2020-06-17 14:05:30 -0700

    fixing up link actions

---
+++ precache-manifest.48bfcc735266ce3191aa3bc0cfdfac86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0904f618451b6e780de92accebb87d09",
+    "url": "https://nih-fmrif.github.io/sharestats/index.html"
+  },
+  {
+    "revision": "434f1f16153dc701ae59",
+    "url": "https://nih-fmrif.github.io/sharestats/static/css/main.5826867c.chunk.css"
+  },
+  {
+    "revision": "9c2d1bab490fecfdf455",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/2.ad35a7ba.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/2.ad35a7ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "434f1f16153dc701ae59",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/main.922d3b7c.chunk.js"
+  },
+  {
+    "revision": "a888652928d39bd29a45",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/runtime-main.dcc49af7.js"
+  }
+]);
