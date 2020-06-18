6d42e1633630b75cb57f7e7a6f9776b55033b4f8
David Siebo <zentraal@gmail.com>
2020-06-17 17:55:49 -0700

    experimenting with chip colors for sharing states

---
+++ precache-manifest.9998d71dd4a5d955198b02a5cf83df55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76cc68a399ab52c34ed50618fa3030cd",
+    "url": "https://nih-fmrif.github.io/sharestats/index.html"
+  },
+  {
+    "revision": "13f3c98999a08212f993",
+    "url": "https://nih-fmrif.github.io/sharestats/static/css/main.5826867c.chunk.css"
+  },
+  {
+    "revision": "3fa31dec912190258017",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/2.9add607f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/2.9add607f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13f3c98999a08212f993",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/main.d95c87e9.chunk.js"
+  },
+  {
+    "revision": "a888652928d39bd29a45",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/runtime-main.dcc49af7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://nih-fmrif.github.io/sharestats/precache-manifest.838c76f5924c4003c3dd9bf7c0eb521a.js"
+  "https://nih-fmrif.github.io/sharestats/precache-manifest.9998d71dd4a5d955198b02a5cf83df55.js"
 );
 
 self.addEventListener('message', (event) => {
