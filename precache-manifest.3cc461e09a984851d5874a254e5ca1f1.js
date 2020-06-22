7ec664ca1c2bbb66c017388425d33828b9a65e44
David Siebo <zentraal@gmail.com>
2020-06-22 09:53:21 -0700

    graph for investigator scores

---
+++ precache-manifest.3cc461e09a984851d5874a254e5ca1f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "713dbecef4c277faa4b605f743abd957",
+    "url": "https://nih-fmrif.github.io/sharestats/index.html"
+  },
+  {
+    "revision": "8cda1f78641b076f7b4c",
+    "url": "https://nih-fmrif.github.io/sharestats/static/css/main.e4e35b74.chunk.css"
+  },
+  {
+    "revision": "0e61f9a993b19400ce40",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/2.2e0c32e7.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/2.2e0c32e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cda1f78641b076f7b4c",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/main.1dea6d2b.chunk.js"
+  },
+  {
+    "revision": "a888652928d39bd29a45",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/runtime-main.dcc49af7.js"
+  }
+]);
