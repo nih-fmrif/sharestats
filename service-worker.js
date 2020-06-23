b3cd3c7fcf70e605b7eff1198b053dd751f4415d
David Siebo <zentraal@gmail.com>
2020-06-22 18:32:31 -0700

    catching vars to use for API call

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://nih-fmrif.github.io/sharestats/precache-manifest.e95876e65fa10a3d56f1a1de34107602.js"
+  "https://nih-fmrif.github.io/sharestats/precache-manifest.e7b5a3718d693c132076c2cdb88695cd.js"
 );
 
 self.addEventListener('message', (event) => {
