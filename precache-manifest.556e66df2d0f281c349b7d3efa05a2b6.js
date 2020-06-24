ca8d8d20fa8412db9b7d27cab1dfc2415b485cff
David Siebo <zentraal@gmail.com>
2020-06-24 00:45:24 -0700

    add tooltip code to fixed lookup dict

---
+++ precache-manifest.556e66df2d0f281c349b7d3efa05a2b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02530bf7a1901762dd18c2d96a443349",
+    "url": "https://nih-fmrif.github.io/sharestats/index.html"
+  },
+  {
+    "revision": "a186cc01d2fccbbaffab",
+    "url": "https://nih-fmrif.github.io/sharestats/static/css/main.e4e35b74.chunk.css"
+  },
+  {
+    "revision": "86891f21a84c58bea4c9",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/2.5af57b62.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/2.5af57b62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a186cc01d2fccbbaffab",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/main.0eba66d5.chunk.js"
+  },
+  {
+    "revision": "a888652928d39bd29a45",
+    "url": "https://nih-fmrif.github.io/sharestats/static/js/runtime-main.dcc49af7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://nih-fmrif.github.io/sharestats/precache-manifest.4466af435919aa0da394e26c4198dd71.js"
+  "https://nih-fmrif.github.io/sharestats/precache-manifest.556e66df2d0f281c349b7d3efa05a2b6.js"
 );
 
 self.addEventListener('message', (event) => {
